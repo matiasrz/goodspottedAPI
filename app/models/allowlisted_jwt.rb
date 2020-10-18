55613f9426189512d290ab5c72b23d2a3311928a
Matias C. Rojas Z <rojasz.ma@gmail.com>
2020-10-18 12:58:44 +0200

    Callback: Handling execution of revoke JWT worker.

--- app/models/allowlisted_jwt.rb
+++ app/models/allowlisted_jwt.rb
@@ -1,3 +1,11 @@
 class AllowlistedJwt < ApplicationRecord
+  after_create_commit :revocation_schedule
+
   belongs_to :user
+
+  private
+
+  def revocation_schedule
+    RevokeJwtWorker.perform_async(id) if id.present?
+  end
 end
